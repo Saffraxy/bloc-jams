52e848d680eff49ffc91bd856e57ed917c43cda6
Sylvia Sievers <ssievers3@gmail.com>
2016-12-06 12:02:22 -0500

    checkpoint-23-DOMevents completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function() {
-
-             var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+ 
+var animatePoints = function(points) {           
+     
     /*Refactor the individual style calls of the landing.js script to be a single function named revealPoint that:
     takes a single argument: the index of the points class node element, and
     gets called in a for loop.*/
@@ -16,3 +17,22 @@ var animatePoints = function() {
         revealPoint(i);
     };
 };
+
+ window.onload = function() {
+     // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     
+      var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+      window.addEventListener('scroll', function(event) {
+      console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+          
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          animatePoints(pointsArray);
+      }
+     });
+ }
