252d3e78a57279e17901edb118327fa6bbace4dd
Sylvia Sievers <ssievers3@gmail.com>
2016-12-06 10:55:07 -0500

    assignment-22-DOMScript completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,18 @@
 var animatePoints = function() {
 
              var points = document.getElementsByClassName('point');
-
-             var revealFirstPoint = function() {
-                 points[0].style.opacity = 1;
-                 points[0].style.transform = "scaleX(1) translateY(0)";
-                 points[0].style.msTransform = "scaleX(1) translateY(0)";
-                 points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-             };
-
-             var revealSecondPoint = function() {
-                 points[1].style.opacity = 1;
-                 points[1].style.transform = "scaleX(1) translateY(0)";
-                 points[1].style.msTransform = "scaleX(1) translateY(0)";
-                 points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-             };
-
-             var revealThirdPoint = function() {
-                 points[2].style.opacity = 1;
-                 points[2].style.transform = "scaleX(1) translateY(0)";
-                 points[2].style.msTransform = "scaleX(1) translateY(0)";
-                 points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-             };
-
-             revealFirstPoint();
-             revealSecondPoint();
-             revealThirdPoint();
- 
-             };
+    /*Refactor the individual style calls of the landing.js script to be a single function named revealPoint that:
+    takes a single argument: the index of the points class node element, and
+    gets called in a for loop.*/
+    
+    var revealPoint = function(indx) {
+             points[indx].style.opacity = 1;
+             points[indx].style.transform = "scaleX(1) translateY(0)";
+             points[indx].style.msTransform = "scaleX(1) translateY(0)";
+             points[indx].style.WebkitTransform = "scaleX(1) translateY(0)"; 
+    };
+    
+    for (var i=0; i < points.length; i++) {
+        revealPoint(i);
+    };
+};
